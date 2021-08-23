9a95d6f2384dac676897e844ca184c2a622ed899
adriantwardy <adriantwardawski@gmail.com>
2021-08-23 19:09:33 +0200

    first commit

---
+++ WebScrapperStock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebScrapperStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stockScraper = new StockScraper();
+
+            /*var stocks =*/ stockScraper.GetStocks();
+
+            //foreach (var stock in stocks)
+            //{
+            //    Console.WriteLine($"Nazwa: {stock.Walor}");
+            //}
+
+        }
+    }
+}
---
+++ WebScrapperStock/StockScraper.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebScrapperStock
+{
+    class StockScraper
+    {
+        private const string BaseUrl = "https://www.bankier.pl/gielda/notowania/akcje";
+
+        public void GetStocks()
+        {
+            var web = new HtmlWeb();
+            var document = web.Load(BaseUrl);
+            var tableRows = document.QuerySelectorAll("table tr").Skip(1).Skip(11);
+
+            foreach (var tableRow in tableRows)
+            {
+                var tds = tableRow.QuerySelectorAll("td a");
+                var walor = tds[0].InnerText;
+
+                var tds2 = tableRow.QuerySelectorAll("td");
+                var kurs = tds2[1].InnerText;
+
+                var zmiana = tds2[2].InnerText;
+
+                Console.WriteLine($"Nazwa: {walor}, Kurs: {kurs}, Zmiana: {zmiana}");
+
+                
+            }
+        }
+    }
+}
